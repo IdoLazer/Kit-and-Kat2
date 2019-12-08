f211b614393e382a770f2a9a987691dd5c3bcc3d
hagar-dolev <hagar.dolev@gmail.com>
2019-12-08 20:36:59 +0200

    adding progress bar

---
+++ Assets/Scripts/ProgressBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    public GameObject ball;
+
+    private Vector3 offSet = new Vector3(0, -25, 0);
+    public Slider progressBar;
+
+
+    void Update()
+    {
+        progressBar.value = ball.GetComponent<BallController>().mHealth / 100;
+        Vector3 newPos = Camera.main.WorldToScreenPoint(ball.transform.position);
+        progressBar.transform.position = newPos + offSet;
+
+    }
+}
